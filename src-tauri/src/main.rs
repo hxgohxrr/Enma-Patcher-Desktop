30d6dfe5ed6084886d7ab49f1ca69bca3661634c
NullOnRise <112127708+NullOnRise@users.noreply.github.com>
2026-09-09 13:14:48 +0200

    cargo fmt

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,4 +1,7 @@
-#![cfg_attr(all(not(debug_assertions), target_os = "windows"), windows_subsystem = "windows")]
+#![cfg_attr(
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
+)]
 
 use std::collections::{HashMap, HashSet};
 use std::fs::{self, File};
